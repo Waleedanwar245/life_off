8ab2fd225c226b3aa3ae0357a487c1d7d081e93c
Ali Raza <aliraza.12748g@gmail.com>
2025-05-11 23:11:16 +0500

    t

--- src/app/components/utils/BASE_URL.ts
+++ src/app/components/utils/BASE_URL.ts
@@ -1,3 +1,1 @@
-export const API_URL = typeof window !== "undefined"
-  ? process.env.NEXT_PUBLIC_API_URL || "https://liveoffcoupon.com/api"
-  : "https://liveoffcoupon.com/api"
+export const API_URL =  "https://liveoffcoupon.com/api"
